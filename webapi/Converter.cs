da24fde88da594b8ef6bcce0888fa5a84496f55b
VladislavSTO <100lnikov.vladislav@gmail.com>
2023-06-21 00:27:16 +0500

    Шляпа

--- webapi/Converter.cs
+++ webapi/Converter.cs
@@ -8,14 +8,23 @@ namespace webapi
         // Converts base64 encoded string to byte[].
         public override byte[] Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            short[] sByteArray = JsonSerializer.Deserialize<short[]>(ref reader);
-            byte[] value = new byte[sByteArray.Length];
-            for (int i = 0; i < sByteArray.Length; i++)
+            try
             {
-                value[i] = (byte)sByteArray[i];
-            }
 
-            return value;
+
+                short[] sByteArray = JsonSerializer.Deserialize<short[]>(ref reader);
+                byte[] value = new byte[sByteArray.Length];
+                for (int i = 0; i < sByteArray.Length; i++)
+                {
+                    value[i] = (byte)sByteArray[i];
+                }
+
+                return value;
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         public override void Write(Utf8JsonWriter writer, byte[] value, JsonSerializerOptions options)
